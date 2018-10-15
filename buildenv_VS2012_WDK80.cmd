33f995d2568224eb600931c50dfa9b6185942d22
Alexander Schulz <code@schulzalex.de>
2018-10-15 23:30:51 +0200

    prepare buildenv for VS2012 and VS2017

---
+++ buildenv_VS2012_WDK80.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+set VS=C:\Program Files (x86)\Microsoft Visual Studio 11.0
+set KIT=C:\Program Files (x86)\Windows Kits\8.0
+
+cd ..
+
+cmd /k
---
+++ buildenv_VS2017_WDK10.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+set VS=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community
+set KIT=C:\Program Files (x86)\Windows Kits\10
+
+cd ..
+
+cmd /k
