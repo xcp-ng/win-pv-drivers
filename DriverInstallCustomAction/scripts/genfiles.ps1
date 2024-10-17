870d53349a06f4ad6a2e644ba76b57dd2b4bacac
Tu Dinh <ngoc-tu.dinh@vates.tech>
2024-10-17 12:54:49 +0200

    generate file info from scripts

--- DriverInstallCustomAction/scripts/genfiles.ps1
+++ DriverInstallCustomAction/scripts/genfiles.ps1
@@ -54,22 +54,7 @@ using System.Reflection;
 [assembly:AssemblyCompany("${Env:VENDOR_NAME}")]
 [assembly:AssemblyProduct("${Env:PRODUCT_NAME}")]
 [assembly:AssemblyCopyright("${Env:COPYRIGHT}")]
-[assembly:AssemblyTitle("Xen PV driver utility library")]
-
-namespace XenDriverUtils {
-    internal static class Version {
-        public const string VendorName = "${Env:VENDOR_NAME}";
-        public const string ProductName = "${Env:PRODUCT_NAME}";
-        public const string VendorPrefix = "${Env:VENDOR_PREFIX}";
-        public const string VendorDeviceId = "${Env:VENDOR_DEVICE_ID}";
-        public const string Copyright = "${Env:COPYRIGHT}";
-
-        public const string MajorVersion = "${Env:MAJOR_VERSION}";
-        public const string MinorVersion = "${Env:MINOR_VERSION}";
-        public const string MicroVersion = "${Env:MICRO_VERSION}";
-        public const string BuildNumber = "${Env:BUILD_NUMBER}";
-    }
-}
+[assembly:AssemblyTitle("Xen PV driver installation library")]
 "@
 
 if ($NewBranding -ne $OldBranding) {
