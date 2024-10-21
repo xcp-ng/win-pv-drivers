c0bfbb8eb50e7c845210b0ed6ad63772acb00ded
Tu Dinh <ngoc-tu.dinh@vates.tech>
2024-10-21 18:43:40 +0200

    tweak genfiles.ps1

--- DriverInstallCustomAction/scripts/genfiles.ps1
+++ DriverInstallCustomAction/scripts/genfiles.ps1
@@ -1,6 +1,6 @@
 [CmdletBinding()]
 param (
-    [Parameter()]
+    [Parameter(Mandatory)]
     [string]$ProjectDir
 )
 
--- installer/scripts/genfiles.ps1
+++ installer/scripts/genfiles.ps1
@@ -1,6 +1,6 @@
 [CmdletBinding()]
 param (
-    [Parameter()]
+    [Parameter(Mandatory)]
     [string]$ProjectDir
 )
 
@@ -9,23 +9,22 @@ param (
 
 $BrandingFile = "$ProjectDir\Branding.wxi"
 $OldBranding = Get-Content -Raw $BrandingFile -ErrorAction Ignore
-$NewBranding = `
-@"
+$NewBranding = @"
 <?xml version="1.0" encoding="utf-8"?>
 <Include xmlns="http://wixtoolset.org/schemas/v4/wxs">
-  <?define VENDOR_NAME="${Env:VENDOR_NAME}"?>
-  <?define PRODUCT_NAME="${Env:PRODUCT_NAME}"?>
-  <?define VENDOR_PREFIX="${Env:VENDOR_PREFIX}"?>
-  <?define VENDOR_DEVICE_ID="${Env:VENDOR_DEVICE_ID}"?>
-  <?define COPYRIGHT="${Env:COPYRIGHT}"?>
+    <?define VENDOR_NAME="${Env:VENDOR_NAME}"?>
+    <?define PRODUCT_NAME="${Env:PRODUCT_NAME}"?>
+    <?define VENDOR_PREFIX="${Env:VENDOR_PREFIX}"?>
+    <?define VENDOR_DEVICE_ID="${Env:VENDOR_DEVICE_ID}"?>
+    <?define COPYRIGHT="${Env:COPYRIGHT}"?>
 
-  <?define MAJOR_VERSION="${Env:MAJOR_VERSION}"?>
-  <?define MINOR_VERSION="${Env:MINOR_VERSION}"?>
-  <?define MICRO_VERSION="${Env:MICRO_VERSION}"?>
-  <?define BUILD_NUMBER="${Env:BUILD_NUMBER}"?>
+    <?define MAJOR_VERSION="${Env:MAJOR_VERSION}"?>
+    <?define MINOR_VERSION="${Env:MINOR_VERSION}"?>
+    <?define MICRO_VERSION="${Env:MICRO_VERSION}"?>
+    <?define BUILD_NUMBER="${Env:BUILD_NUMBER}"?>
 
-  <?define MSI_UPGRADE_CODE_X86="${Env:MSI_UPGRADE_CODE_X86}"?>
-  <?define MSI_UPGRADE_CODE_X64="${Env:MSI_UPGRADE_CODE_X64}"?>
+    <?define MSI_UPGRADE_CODE_X86="${Env:MSI_UPGRADE_CODE_X86}"?>
+    <?define MSI_UPGRADE_CODE_X64="${Env:MSI_UPGRADE_CODE_X64}"?>
 </Include>
 "@
 
