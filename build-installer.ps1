4b8ff486c9fde37d21abdf768db96b9e88c06407
Tu Dinh <ngoc-tu.dinh@vates.tech>
2024-10-23 12:38:16 +0200

    add installer build script

---
+++ build-installer.ps1
@@ -0,0 +1,30 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]$Target = "Build",
+    [Parameter(Mandatory)]
+    [string]$Configuration,
+    [Parameter(Mandatory)]
+    [string]$Platform,
+    [Parameter()]
+    [string]$OutDir = "$PSScriptRoot\package"
+)
+
+$ErrorActionPreference = "Stop"
+
+msbuild.exe "$PSScriptRoot\installer\installer.sln" /t:$Target /p:Configuration=$Configuration /p:Platform=$Platform
+if ($LASTEXITCODE -ne 0) {
+    throw "MSBuild failed with error $LASTEXITCODE"
+}
+
+if ($Target -ine "Clean") {
+    New-Item -Path $OutDir -ItemType Directory -Force
+    Copy-Item -Path "$PSScriptRoot\installer\bin\$Platform\$Configuration\en-US\*" -Destination $OutDir\
+
+    $XenCleanOutDir = "$OutDir\XenClean\$Platform"
+    New-Item -Path $XenCleanOutDir -ItemType Directory -Force
+    Copy-Item -Path "$PSScriptRoot\XenClean\bin\$Platform\$Configuration\net462\Invoke-XenClean.ps1" -Destination $XenCleanOutDir\
+
+    New-Item -Path $XenCleanOutDir\bin -ItemType Directory -Force
+    Copy-Item -Path "$PSScriptRoot\XenClean\bin\$Platform\$Configuration\net462\*" -Exclude Invoke-XenClean.ps1 -Destination $XenCleanOutDir\bin\
+}
