15c203256adbc86eaab1899140216c17cf7601ce
Tu Dinh <ngoc-tu.dinh@vates.tech>
2024-12-05 16:13:07 +0100

    add ServiceDelete CAs
    Signed-off-by: Tu Dinh <ngoc-tu.dinh@vates.tech>

--- DriverInstallCustomAction/CleanupActions.cs
+++ DriverInstallCustomAction/CleanupActions.cs
@@ -1,3 +1,4 @@
+using Windows.Win32;
 using WixToolset.Dtf.WindowsInstaller;
 using XenDriverUtils;
 
@@ -22,6 +23,22 @@ namespace XenInstCA {
         public static ActionResult XenfiltReset(Session session) {
             using var logScope = new LoggerScope(new MsiSessionLogger(session));
             XenCleanup.XenfiltReset();
+            return ActionResult.Success;
+        }
+
+        [CustomAction]
+        public static ActionResult XenServiceDelete(Session session) {
+            using var logScope = new LoggerScope(new MsiSessionLogger(session));
+
+            if (!session.CustomActionData.TryGetValue("Services", out var services))
+                return ActionResult.Success;
+            var serviceList = services.Split(',');
+
+            using var scm = PInvoke.OpenSCManager((string)null, (string)null, PInvoke.SC_MANAGER_ALL_ACCESS);
+            foreach (var serviceName in serviceList) {
+                XenCleanup.DeleteService(scm, serviceName);
+            }
+
             return ActionResult.Success;
         }
     }
