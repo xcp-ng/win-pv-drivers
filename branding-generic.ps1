8048724e6ef9412bac95b0ae287d4db5ec807d3b
Tu Dinh <ngoc-tu.dinh@vates.tech>
2025-02-07 20:23:06 +0100

    branding: fix xeniface missing OBJECT_PREFIX
    Signed-off-by: Tu Dinh <ngoc-tu.dinh@vates.tech>

--- branding-generic.ps1
+++ branding-generic.ps1
@@ -5,25 +5,30 @@ function Get-PackageVersion {
         [string]$PackageName = 'Product'
     )
     $Version = $PackageVersions[$PackageName]
-    if ([string]::IsNullOrEmpty($Version)) {
+    if (!$Version) {
         throw "Cannot get $PackageName version"
     }
     return [version]::Parse($Version)
 }
 
-if ([string]::IsNullOrEmpty($Env:VENDOR_NAME)) {
+if (!$Env:VENDOR_NAME) {
     $Env:VENDOR_NAME = 'Xen Project'
 }
 
-if ([string]::IsNullOrEmpty($Env:VENDOR_PREFIX)) {
+if (!$Env:VENDOR_PREFIX) {
     $Env:VENDOR_PREFIX = 'XP'
 }
 
-if ([string]::IsNullOrEmpty($Env:PRODUCT_NAME)) {
+if (!$Env:PRODUCT_NAME) {
     $Env:PRODUCT_NAME = 'Xen'
 }
 
-if ([string]::IsNullOrEmpty($Env:COPYRIGHT)) {
+# xeniface
+if (!$Env:OBJECT_PREFIX) {
+    $Env:OBJECT_PREFIX = 'XenProject'
+}
+
+if (!$Env:COPYRIGHT) {
     $Env:COPYRIGHT = 'Copyright (c) Xen Project.'
 }
 
@@ -65,10 +70,10 @@ if ($null -eq $PackageVersions['XenBootFix']) {
     $PackageVersions['XenBootFix'] = $PackageVersions['Product']
 }
 
-if ([string]::IsNullOrEmpty($Env:MSI_UPGRADE_CODE_X86)) {
+if (!$Env:MSI_UPGRADE_CODE_X86) {
     $Env:MSI_UPGRADE_CODE_X86 = '{10828840-D8A9-4953-B44A-1F1D3CD7ECB0}'
 }
 
-if ([string]::IsNullOrEmpty($Env:MSI_UPGRADE_CODE_X64)) {
+if (!$Env:MSI_UPGRADE_CODE_X64) {
     $Env:MSI_UPGRADE_CODE_X64 = '{D60FED1E-316C-41B0-B7A5-E44951A82618}'
 }
