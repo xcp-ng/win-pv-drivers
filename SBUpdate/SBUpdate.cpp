15d2e202eccc999a84ae11719408d2fd328618d3
Tu Dinh <ngoc-tu.dinh@vates.tech>
2025-02-12 23:41:45 +0100

    SBUpdate: suspend BitLocker on success if TPM-bound
    Signed-off-by: Tu Dinh <ngoc-tu.dinh@vates.tech>

--- SBUpdate/SBUpdate.cpp
+++ SBUpdate/SBUpdate.cpp
@@ -1,30 +1,8 @@
-#include <cstdio>
-#include <system_error>
-#include <algorithm>
-#include <memory>
-#include <array>
-#include <vector>
-#include <atlsecurity.h>
-#include <Windows.h>
+#include "SBUpdate.hpp"
 
 #ifndef UNICODE
 #error "SBUpdate assumes Unicode!"
 #endif
-
-// Windows headers don't provide definitions for these attributes, so use our own
-static constexpr DWORD SBUPDATE_VARIABLE_ATTRIBUTE_NON_VOLATILE = 0x00000001;
-static constexpr DWORD SBUPDATE_VARIABLE_ATTRIBUTE_BOOTSERVICE_ACCESS = 0x00000002;
-static constexpr DWORD SBUPDATE_VARIABLE_ATTRIBUTE_RUNTIME_ACCESS = 0x00000004;
-static constexpr DWORD SBUPDATE_VARIABLE_ATTRIBUTE_HARDWARE_ERROR_RECORD = 0x00000008;
-static constexpr DWORD SBUPDATE_VARIABLE_ATTRIBUTE_AUTHENTICATED_WRITE_ACCESS = 0x00000010;
-static constexpr DWORD SBUPDATE_VARIABLE_ATTRIBUTE_TIME_BASED_AUTHENTICATED_WRITE_ACCESS = 0x00000020;
-static constexpr DWORD SBUPDATE_VARIABLE_ATTRIBUTE_APPEND_WRITE = 0x00000040;
-
-struct SecureBootVariable {
-    const wchar_t* VariableName;
-    const wchar_t* VariableGuid;
-    const DWORD VariableAttributes;
-};
 
 static const std::array<SecureBootVariable, 3> AllowedVariables = {
     SecureBootVariable{
@@ -44,11 +22,11 @@ static const std::array<SecureBootVariable, 3> AllowedVariables = {
     },
 };
 
-static void PrintUsage(const wchar_t* name) {
+static void PrintUsage(_In_ const wchar_t* name) {
     wprintf(L"Usage: %s [--append] <variable name> <update file path>\n", name);
 }
 
-static std::unique_ptr<FILE, decltype(&fclose)> OpenFile(const wchar_t* path, const wchar_t* mode) {
+static std::unique_ptr<FILE, decltype(&fclose)> OpenFile(_In_ const wchar_t* path, _In_ const wchar_t* mode) {
     FILE* file;
     auto err = _wfopen_s(&file, path, mode);
     if (err) {
@@ -66,10 +44,177 @@ static void EnablePrivileges() {
         throw std::system_error(GetLastError(), std::system_category(), "EnablePrivilege(SE_SYSTEM_ENVIRONMENT_NAME)");
 }
 
+static void ReadVariableBlob(_In_ const wchar_t* blobpath, _Out_ std::vector<uint8_t>& blob) {
+    auto blobfile = OpenFile(blobpath, L"rb");
+
+    if (fseek(blobfile.get(), 0, SEEK_END)) {
+        throw std::system_error(errno, std::generic_category(), "fseek(blobfile)");
+    }
+
+    auto fsize = ftell(blobfile.get());
+    if (fsize < 0) {
+        throw std::system_error(errno, std::generic_category(), "ftell(blobfile)");
+    }
+    else if (fsize > 0x100000) {
+        // arbitrary 1MB size limit
+        throw std::runtime_error("Input file too big");
+    }
+    wprintf(L"Input file is %d bytes\n", fsize);
+
+    if (fseek(blobfile.get(), 0, SEEK_SET) < 0) {
+        throw std::system_error(errno, std::generic_category(), "fseek(blobfile)");
+    }
+
+    blob.resize(fsize);
+    int pos = 0;
+    while (pos < fsize) {
+        auto readcount = fread_s(&blob[pos], blob.size() - pos, 1, static_cast<size_t>(fsize) - pos, blobfile.get());
+        wprintf(L"Read %zu\n", readcount);
+        if (ferror(blobfile.get())) {
+            throw std::system_error(errno, std::generic_category(), "fread(blobfile)");
+        }
+        else if (!readcount) {
+            throw std::runtime_error("Input file truncated");
+        }
+        pos += static_cast<int>(readcount);
+    }
+}
+
+static void DoInitializeCom() {
+    auto hr = CoInitializeEx(NULL, COINIT_MULTITHREADED);
+    if (FAILED(hr)) {
+        throw std::system_error(hr, std::system_category(), "CoInitializeEx");
+    }
+
+    hr = CoInitializeSecurity(NULL, -1, NULL, NULL, RPC_C_AUTHN_LEVEL_DEFAULT, RPC_C_IMP_LEVEL_IMPERSONATE, NULL, EOAC_NONE, NULL);
+    if (FAILED(hr)) {
+        throw std::system_error(hr, std::system_category(), "CoInitializeSecurity");
+    }
+}
+
+static void OpenWbemServices(_In_ const wchar_t* resource, _Outptr_ IWbemServices** outServices) {
+    CComPtr<IWbemLocator> locator;
+    HRESULT hr = locator.CoCreateInstance(CLSID_WbemLocator);
+    if (FAILED(hr)) {
+        throw std::system_error(hr, std::system_category(), "CoCreateInstance(CLSID_WbemLocator)");
+    }
+
+    hr = locator->ConnectServer(_bstr_t(resource), NULL, NULL, NULL, 0, NULL, NULL, outServices);
+    if (FAILED(hr)) {
+        throw std::system_error(hr, std::system_category(), "ConnectServer");
+    }
+}
+
+static bool FindFveOsDrive(_In_ IWbemServices* services, _Outptr_ IWbemClassObject** outObject) {
+    *outObject = nullptr;
+
+    HRESULT hr = CoSetProxyBlanket(services, RPC_C_AUTHN_WINNT, RPC_C_AUTHZ_NONE, NULL, RPC_C_AUTHN_LEVEL_CALL, RPC_C_IMP_LEVEL_IMPERSONATE, NULL, EOAC_NONE);
+    if (FAILED(hr)) {
+        throw std::system_error(hr, std::system_category(), "CoSetProxyBlanket");
+    }
+
+    CComPtr<IEnumWbemClassObject> enumerator;
+    hr = services->ExecQuery(_bstr_t(L"WQL"), _bstr_t(L"select * from Win32_EncryptableVolume where VolumeType = 0"), WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY, NULL, &enumerator);
+    if (FAILED(hr)) {
+        throw std::system_error(hr, std::system_category(), "services->ExecQuery(Win32_EncryptableVolume)");
+    }
+
+    ULONG returned = 0;
+    hr = enumerator->Next(WBEM_INFINITE, 1, outObject, &returned);
+    if (FAILED(hr)) {
+        *outObject = nullptr;
+        throw std::system_error(hr, std::system_category(), "Next(Win32_EncryptableVolume)");
+    }
+
+    return !!*outObject;
+}
+
+static void GetWbemInstancePath(_In_ IWbemClassObject* obj, _Outptr_ BSTR* output) {
+    VARIANT instancePath;
+    HRESULT hr;
+
+    VariantInit(&instancePath);
+    hr = obj->Get(L"__PATH", 0, &instancePath, nullptr, nullptr);
+    if (FAILED(hr)) {
+        throw std::system_error(hr, std::system_category(), "fveOsDrive->Get(__PATH)");
+    }
+
+    if (instancePath.vt != VT_BSTR) {
+        throw std::runtime_error("Unknown fveInstancePath type");
+    }
+
+    *output = instancePath.bstrVal;
+}
+
+static bool FveIsSecureBootBound(_In_ IWbemServices* services, _In_ IWbemClassObject* win32_EncryptableVolumeClass, _In_ BSTR instancePath) {
+    HRESULT hr;
+
+    CComPtr<IWbemClassObject> getSecureBootBindingState_Out;
+    hr = win32_EncryptableVolumeClass->GetMethod(FveGetSecureBootBindingState, 0, nullptr, &getSecureBootBindingState_Out);
+    if (FAILED(hr)) {
+        throw std::system_error(hr, std::system_category(), "GetMethod(GetSecureBootBindingState)");
+    }
+
+    CComPtr<IWbemClassObject> outParams;
+    CComPtr<IWbemCallResult> result;
+    hr = services->ExecMethod(instancePath, _bstr_t(FveGetSecureBootBindingState), 0, nullptr, nullptr, &outParams, &result);
+    if (FAILED(hr)) {
+        throw std::system_error(hr, std::system_category(), "ExecMethod(GetSecureBootBindingState)");
+    }
+
+    long status;
+    hr = result->GetCallStatus(WBEM_INFINITE, &status);
+    if (FAILED(hr)) {
+        throw std::system_error(hr, std::system_category(), "result->GetCallStatus");
+    }
+    if (status) {
+        throw std::system_error(hr, std::system_category(), "GetSecureBootBindingState");
+    }
+
+    CComVariant bindingState;
+    hr = outParams->Get(L"BindingState", 0, &bindingState, nullptr, nullptr);
+    if (FAILED(hr)) {
+        throw std::system_error(hr, std::system_category(), "outParams->Get(BindingState)");
+    }
+
+    if (bindingState.vt != VT_I4) {
+        throw std::runtime_error("Unknown bindingState type");
+    }
+
+    wprintf(L"Binding state %u\n", bindingState.lVal);
+    return bindingState.ulVal == FveSecureBootBindingStateBound;
+}
+
+static void FveSuspend(_In_ IWbemServices* services, _In_ IWbemClassObject* win32_EncryptableVolumeClass, BSTR fveInstancePath) {
+    HRESULT hr;
+
+    CComPtr<IWbemClassObject> disableKeyProtectors_In;
+    hr = win32_EncryptableVolumeClass->GetMethod(FveDisableKeyProtectors, 0, &disableKeyProtectors_In, nullptr);
+    if (FAILED(hr)) {
+        throw std::system_error(hr, std::system_category(), "win32_EncryptableVolumeClass->GetMethod(DisableKeyProtectors)");
+    }
+
+    CComPtr<IWbemCallResult> result;
+    hr = services->ExecMethod(fveInstancePath, _bstr_t(FveDisableKeyProtectors), 0, nullptr, disableKeyProtectors_In, nullptr, &result);
+    if (FAILED(hr)) {
+        throw std::system_error(hr, std::system_category(), "ExecMethod(DisableKeyProtectors)");
+    }
+
+    long status;
+    hr = result->GetCallStatus(WBEM_INFINITE, &status);
+    if (FAILED(hr)) {
+        throw std::system_error(hr, std::system_category(), "result->GetCallStatus");
+    }
+    if (status) {
+        throw std::system_error(hr, std::system_category(), "DisableKeyProtectors");
+    }
+}
+
 int wmain(int argc, wchar_t** argv) {
     wchar_t* varname = nullptr;
     wchar_t* blobpath = nullptr;
     bool append = false;
+    HRESULT hr;
 
     for (int i = 1; i < argc; i++) {
         if (CompareStringOrdinal(L"--append", -1, argv[i], -1, TRUE) == CSTR_EQUAL) {
@@ -96,45 +241,49 @@ int wmain(int argc, wchar_t** argv) {
             throw std::runtime_error("Not running on UEFI system");
         }
 
+        CComPtr<IWbemServices> services;
+        CComPtr<IWbemClassObject> win32_EncryptableVolumeClass;
+        CComBSTR fveInstancePath;
+        bool isBound = false;
+        {
+            CRegKey winPeKey;
+            if (winPeKey.Open(HKEY_LOCAL_MACHINE, L"SYSTEM\\CurrentControlSet\\Control\\MiniNT", KEY_READ) != ERROR_SUCCESS) {
+                DoInitializeCom();
+                OpenWbemServices(FveNamespace, &services);
+
+                // get a reference to the Win32_EncryptableVolume class object so that we could get method references
+                hr = services->GetObjectW(_bstr_t(L"Win32_EncryptableVolume"), 0, NULL, &win32_EncryptableVolumeClass, NULL);
+
+                CComPtr<IWbemClassObject> fveOsDrive;
+                FindFveOsDrive(services, &fveOsDrive);
+                if (fveOsDrive) {
+                    wprintf(L"Found active BitLocker OS volume\n");
+
+                    // describe the obtained Win32_EncryptableVolume instance object
+                    CComBSTR fveOsDriveText;
+                    auto hr = fveOsDrive->GetObjectText(0, &fveOsDriveText);
+                    if (FAILED(hr)) {
+                        throw std::system_error(hr, std::system_category(), "fveOsDrive->GetObjectText");
+                    }
+                    wprintf(L"%s\n", static_cast<wchar_t*>(fveOsDriveText));
+
+                    GetWbemInstancePath(fveOsDrive, &fveInstancePath);
+                    isBound = FveIsSecureBootBound(services, win32_EncryptableVolumeClass, fveInstancePath);
+                    if (isBound) {
+                        wprintf(L"BitLocker is Secure Boot-bound, needs rebinding\n");
+                    }
+                }
+            }
+        }
+
         auto it = std::find_if(AllowedVariables.begin(), AllowedVariables.end(), [=](const auto& x) { return !wcscmp(varname, x.VariableName); });
         if (it == AllowedVariables.end()) {
             throw std::runtime_error("Invalid variable");
         }
         wprintf(L"Variable %s %s attr 0x%x\n", it->VariableName, it->VariableGuid, it->VariableAttributes);
 
-        auto blobfile = OpenFile(blobpath, L"rb");
-
-        if (fseek(blobfile.get(), 0, SEEK_END)) {
-            throw std::system_error(errno, std::generic_category(), "fseek(blobfile)");
-        }
-
-        auto fsize = ftell(blobfile.get());
-        if (fsize < 0) {
-            throw std::system_error(errno, std::generic_category(), "ftell(blobfile)");
-        }
-        else if (fsize > 0x100000) {
-            // arbitrary 1MB size limit
-            throw std::runtime_error("Input file too big");
-        }
-        wprintf(L"Input file is %d bytes\n", fsize);
-
-        if (fseek(blobfile.get(), 0, SEEK_SET) < 0) {
-            throw std::system_error(errno, std::generic_category(), "fseek(blobfile)");
-        }
-
-        std::vector<uint8_t> blob(fsize);
-        int pos = 0;
-        while (pos < fsize) {
-            auto readcount = fread_s(&blob[pos], blob.size() - pos, 1, static_cast<size_t>(fsize) - pos, blobfile.get());
-            wprintf(L"Read %zu\n", readcount);
-            if (ferror(blobfile.get())) {
-                throw std::system_error(errno, std::generic_category(), "fread(blobfile)");
-            }
-            else if (!readcount) {
-                throw std::runtime_error("Input file truncated");
-            }
-            pos += static_cast<int>(readcount);
-        }
+        std::vector<uint8_t> blob;
+        ReadVariableBlob(blobpath, blob);
 
         wprintf(L"Enabling privileges\n");
         EnablePrivileges();
@@ -143,12 +292,20 @@ int wmain(int argc, wchar_t** argv) {
         if (append) {
             wprintf(L"Append write\n");
             attr |= SBUPDATE_VARIABLE_ATTRIBUTE_APPEND_WRITE;
+            // TODO: check if append write is redundant
         }
         if (!SetFirmwareEnvironmentVariableExW(it->VariableName, it->VariableGuid, blob.data(), static_cast<DWORD>(blob.size()), attr)) {
             throw std::system_error(GetLastError(), std::system_category(), "SetFirmwareEnvironmentVariableExW");
         }
 
-        wprintf(L"Success!\n");
+        if (isBound) {
+            wprintf(L"Suspending BitLocker\n");
+            FveSuspend(services, win32_EncryptableVolumeClass, fveInstancePath);
+            wprintf(L"Success! You must reboot now to update BitLocker\n");
+        }
+        else {
+            wprintf(L"Success!\n");
+        }
     }
     catch (const std::exception& ex) {
         wprintf(L"Error: %S\n", ex.what());
---
+++ SBUpdate/SBUpdate.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <cstdio>
+#include <system_error>
+#include <algorithm>
+#include <memory>
+#include <array>
+#include <vector>
+#include <atlsecurity.h>
+#include <comdef.h>
+#include <WbemIdl.h>
+#include <Windows.h>
+
+// Windows headers don't provide definitions for these attributes, so use our own
+static constexpr DWORD SBUPDATE_VARIABLE_ATTRIBUTE_NON_VOLATILE = 0x00000001;
+static constexpr DWORD SBUPDATE_VARIABLE_ATTRIBUTE_BOOTSERVICE_ACCESS = 0x00000002;
+static constexpr DWORD SBUPDATE_VARIABLE_ATTRIBUTE_RUNTIME_ACCESS = 0x00000004;
+static constexpr DWORD SBUPDATE_VARIABLE_ATTRIBUTE_HARDWARE_ERROR_RECORD = 0x00000008;
+static constexpr DWORD SBUPDATE_VARIABLE_ATTRIBUTE_AUTHENTICATED_WRITE_ACCESS = 0x00000010;
+static constexpr DWORD SBUPDATE_VARIABLE_ATTRIBUTE_TIME_BASED_AUTHENTICATED_WRITE_ACCESS = 0x00000020;
+static constexpr DWORD SBUPDATE_VARIABLE_ATTRIBUTE_APPEND_WRITE = 0x00000040;
+
+struct SecureBootVariable {
+    const wchar_t* VariableName;
+    const wchar_t* VariableGuid;
+    const DWORD VariableAttributes;
+};
+
+// win32_encryptablevolume.mof
+
+static constexpr const wchar_t* FveNamespace = L"Root\\CIMV2\\Security\\MicrosoftVolumeEncryption";
+static constexpr const wchar_t* FveGetSecureBootBindingState = L"GetSecureBootBindingState";
+static constexpr const wchar_t* FveDisableKeyProtectors = L"DisableKeyProtectors";
+
+enum FveSecureBootBindingState {
+    FveSecureBootBindingStateNotPossible,
+    FveSecureBootBindingStateDisabledByPolicy,
+    FveSecureBootBindingStatePossible,
+    FveSecureBootBindingStateBound,
+};
