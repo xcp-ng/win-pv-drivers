f15e3fa1e028d313fc973fd3ad7e05d3be3f8478
Tu Dinh <ngoc-tu.dinh@vates.tech>
2025-04-03 00:20:01 +0200

    XSA-468 cleanup
    Signed-off-by: Tu Dinh <ngoc-tu.dinh@vates.tech>

--- extras/xenstore-wmi.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = "Stop"
-$base = Get-CimInstance -Namespace "root\WMI" -ClassName CitrixXenStoreBase
-$result = Invoke-CimMethod -InputObject $base -MethodName AddSession -Arguments @{Id=$base.InstanceName}
-$session = Get-CimInstance -Namespace "root\WMI" -ClassName CitrixXenStoreSession -Filter "SessionId = $($result.SessionId)"
-try {
-    Invoke-CimMethod -InputObject $session -MethodName SetValue -Arguments @{Pathname="vm-data/foo";value="123"}
-} finally {
-    Invoke-CimMethod -InputObject $session -MethodName EndSession
-}
