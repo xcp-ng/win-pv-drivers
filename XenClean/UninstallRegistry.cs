0d8fb17bca431f0bc09bd1940b5130c62e1567da
Tu Dinh <ngoc-tu.dinh@vates.tech>
2025-07-04 09:51:07 +0200

    call XenfiltReset in XenClean
    Signed-off-by: Tu Dinh <ngoc-tu.dinh@vates.tech>

--- XenClean/UninstallRegistry.cs
+++ XenClean/UninstallRegistry.cs
@@ -4,6 +4,7 @@ namespace XenClean {
     internal class UninstallRegistry {
         public static void Execute() {
             XenCleanup.XenfiltClassCleanup();
+            XenCleanup.XenfiltReset();
             XenCleanup.ResetStartOverride();
             XenCleanup.ResetUnplug();
         }
