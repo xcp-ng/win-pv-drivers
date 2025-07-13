ee1801a2e3a626ca8bbc83130e0fbf3eb149d9db
Tu Dinh <ngoc-tu.dinh@vates.tech>
2025-07-13 20:06:01 +0200

    add Get-DriverSubmodule.ps1
    Signed-off-by: Tu Dinh <ngoc-tu.dinh@vates.tech>

---
+++ scripts/Get-DriverSubmodule.ps1
@@ -0,0 +1,55 @@
+[CmdletBinding()]
+param (
+    [Switch]$Downstream
+)
+
+$submodules = "xenbus", "xencons", "xenhid", "xeniface", "xennet", "xenvbd", "xenvif", "xenvkbd"
+
+foreach ($submodule in $submodules) {
+    # Get submodule head from parent
+    $submodule_head_line = git ls-tree HEAD $submodule
+    $submodule_head = ($submodule_head_line.Trim() -split '\s+')[2]
+
+    # Get current branch
+    $branch = (git -C $submodule rev-parse --abbrev-ref HEAD)
+
+    # Compare with parent repo's commit
+    $commits_since_parent = git -C $submodule log --oneline "$submodule_head..HEAD"
+
+    # Compare with master
+    $downstream_commits = $null
+    if ($Downstream) {
+        # Use git cherry to find commits on the branch that are not on master.
+        # Equivalent commits (by patch-id) are marked with '-'
+        # Unique commits are marked with '+'
+        $cherry_output = git -C $submodule cherry master $branch
+        $downstream_commits = foreach ($line in $cherry_output) {
+            if (-not($line)) {
+                continue
+            }
+            $sign, $sha = $line.Split(' ', 2)
+            $oneline = git -C $submodule log -1 --pretty="format:%h %s" $sha
+            "$sign $oneline"
+        }
+    }
+
+    if (($commits_since_parent) -or ($downstream_commits)) {
+        Write-Output "--- Submodule: $submodule ---"
+        Write-Output "Current branch: $branch"
+        Write-Output ""
+
+        if ($commits_since_parent) {
+            Write-Output "Uncommitted changes from ${submodule_head}:"
+            $commits_since_parent
+            Write-Output ""
+        }
+
+        if ($downstream_commits) {
+            Write-Output "Downstream changes in ${branch}:"
+            $downstream_commits
+            Write-Output ""
+        }
+
+        Write-Output ""
+    }
+}
