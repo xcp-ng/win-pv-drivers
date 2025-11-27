ae3f3b00a8064afce7b62e9daeedc74a6d62dc7d
Tu Dinh <ngoc-tu.dinh@vates.tech>
2025-11-27 12:57:37 +0100

    Revert "generate time provider sbom"
    This reverts commit d35ef1165f579c56f08615bc8686b6ae20a95e01.
    
    As the time provider can be externally-signed, its SBOM must also be
    generated at package build time.

--- build-timeprovider.ps1
+++ build-timeprovider.ps1
@@ -7,9 +7,7 @@ param (
     [string]$Configuration,
     [Parameter(Mandatory)]
     [ValidateSet("x86", "x64")]
-    [string]$Platform,
-    [Parameter()]
-    [switch]$Sbom
+    [string]$Platform
 )
 
 $ErrorActionPreference = "Stop"
@@ -36,13 +34,6 @@ try {
     msbuild.exe @BuildArgs
     if ($LASTEXITCODE -ne 0) {
         throw "MSBuild failed with error $LASTEXITCODE"
-    }
-
-    if ($Sbom) {
-        sbom.exe generate -b .\$Platform\$Configuration -bc . -D true -ps $Env:VENDOR_NAME -pn XenTimeProvider -pv (Get-PackageVersion XenTimeProvider)
-        if ($LASTEXITCODE -ne 0) {
-            throw "sbom-tool failed with error $LASTEXITCODE"
-        }
     }
 }
 finally {
