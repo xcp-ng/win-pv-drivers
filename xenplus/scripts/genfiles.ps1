dbb7b5ed0b147dd2858b565abdc17035d7f6dd3b
Tu Dinh <ngoc-tu.dinh@vates.tech>
2026-06-18 13:47:59 +0200

    xenplus: rework branding injection
    Signed-off-by: Tu Dinh <ngoc-tu.dinh@vates.tech>

--- xenplus/scripts/genfiles.ps1
+++ xenplus/scripts/genfiles.ps1
@@ -9,22 +9,28 @@ param (
 . "$ProjectDir\..\scripts\sign.ps1"
 
 $ver = Get-PackageVersion xenplus
-$productVer = Get-PackageVersion xenplus
+$productVer = Get-PackageVersion Product
+$description = "Xen guest agent for Windows"
 
 $BrandingFile = "$ProjectDir\Branding.cs"
 $OldBranding = Get-Content -Raw $BrandingFile -ErrorAction Ignore
-$NewBranding = `
-    @"
+$NewBranding = @"
 using System.Reflection;
 [assembly:AssemblyVersion("$ver")]
 [assembly:AssemblyInformationalVersion("$productVer")]
 [assembly:AssemblyCompany("${Env:VENDOR_NAME}")]
 [assembly:AssemblyProduct("${Env:PRODUCT_NAME}")]
 [assembly:AssemblyCopyright("${Env:COPYRIGHT}")]
+[assembly:AssemblyTitle("$description")]
 
 namespace XenPlus {
     static class VersionInfo {
+        public const string FileVersion = "$ver";
         public const string ProductVersion = "$productVer";
+        public const string Description = "$description";
+        public const string VendorName = "${Env:VENDOR_NAME}";
+        public const string ProductName = "${Env:PRODUCT_NAME}";
+        public const string Copyright = "${Env:COPYRIGHT}";
     }
 }
 "@
--- xenplus_session/scripts/genfiles.ps1
+++ xenplus_session/scripts/genfiles.ps1
@@ -7,13 +7,18 @@ param (
 . "$ProjectDir\..\branding.ps1"
 . "$ProjectDir\..\scripts\branding-generic.ps1"
 
-$BrandingFile = "$ProjectDir\Branding.inc"
-$OldBranding = Get-Content -Raw $BrandingFile -ErrorAction Ignore
-$NewBranding = @"
+$ver = Get-PackageVersion xenplus
+$productVer = Get-PackageVersion Product
+$description = "Xen session agent"
+
+if ($true) {
+    $BrandingFile = "$ProjectDir\Branding.inc"
+    $OldBranding = Get-Content -Raw $BrandingFile -ErrorAction Ignore
+    $NewBranding = @"
 // VS_VERSION_INFO=1, use that ID as VERSIONINFO or else
 1 VERSIONINFO
- FILEVERSION $((Get-PackageVersion xenplus).ToString().Replace(".", ","))
- PRODUCTVERSION $((Get-PackageVersion Product).ToString().Replace(".", ","))
+ FILEVERSION $($ver.ToString().Replace(".", ","))
+ PRODUCTVERSION $($productVer.ToString().Replace(".", ","))
  FILEFLAGSMASK 0x3fL
 #ifdef _DEBUG
  FILEFLAGS 0x1L
@@ -29,13 +34,13 @@ BEGIN
         BLOCK "200004b0"
         BEGIN
             VALUE "CompanyName", "${Env:VENDOR_NAME}"
-            VALUE "FileDescription", "Xen session agent"
-            VALUE "FileVersion", "$(Get-PackageVersion xenplus)"
+            VALUE "FileDescription", "$description"
+            VALUE "FileVersion", "$ver"
             VALUE "InternalName", "xenplus_session.exe"
             VALUE "LegalCopyright", "${Env:COPYRIGHT}"
             VALUE "OriginalFilename", "xenplus_session.exe"
             VALUE "ProductName", "${Env:PRODUCT_NAME}"
-            VALUE "ProductVersion", "$(Get-PackageVersion Product)"
+            VALUE "ProductVersion", "$productVer"
         END
     END
     BLOCK "VarFileInfo"
@@ -46,7 +51,38 @@ END
 
 "@
 
-if ($NewBranding -ne $OldBranding) {
-    Write-Output "Updating Branding.inc"
-    Set-Content -Path $BrandingFile -Value $NewBranding -NoNewline
+    if ($NewBranding -ne $OldBranding) {
+        Write-Output "Updating Branding.inc"
+        Set-Content -Path $BrandingFile -Value $NewBranding -NoNewline
+    }
+}
+
+if ($true) {
+    $BrandingFile = "$ProjectDir\Branding.cs"
+    $OldBranding = Get-Content -Raw $BrandingFile -ErrorAction Ignore
+    $NewBranding = @"
+using System.Reflection;
+[assembly:AssemblyVersion("$ver")]
+[assembly:AssemblyInformationalVersion("$productVer")]
+[assembly:AssemblyCompany("${Env:VENDOR_NAME}")]
+[assembly:AssemblyProduct("${Env:PRODUCT_NAME}")]
+[assembly:AssemblyCopyright("${Env:COPYRIGHT}")]
+[assembly:AssemblyTitle("$description")]
+
+namespace XenPlus {
+    static class VersionInfo {
+        public const string FileVersion = "$ver";
+        public const string ProductVersion = "$productVer";
+        public const string Description = "$description";
+        public const string VendorName = "${Env:VENDOR_NAME}";
+        public const string ProductName = "${Env:PRODUCT_NAME}";
+        public const string Copyright = "${Env:COPYRIGHT}";
+    }
+}
+"@
+
+    if ($NewBranding -ne $OldBranding) {
+        Write-Output "Updating Branding.cs"
+        Set-Content -Path $BrandingFile -Value $NewBranding -NoNewline
+    }
 }
