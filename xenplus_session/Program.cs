f3db0bc878930e088b418cb35c5d032901a50c48
Tu Dinh <ngoc-tu.dinh@vates.tech>
2026-06-18 14:58:03 +0200

    xenplus_session: remove log file
    Signed-off-by: Tu Dinh <ngoc-tu.dinh@vates.tech>

--- xenplus_session/Program.cs
+++ xenplus_session/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
 [assembly: DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
 
@@ -8,19 +7,12 @@ namespace XenPlus;
 sealed class Program {
     [STAThread]
     static int Main() {
-        var traceLogPath = Path.GetTempFileName();
-        try {
-            Trace.Listeners.Add(new TextWriterTraceListener(traceLogPath));
-
-            using var single = new SingleInstance("{B27A618B-BF63-4DE1-894A-D3A696402174}");
-            if (!single.IsTaken) {
-                return 0;
-            }
-
-            using var mainWindow = new MainWindow();
-            return MessageLoopSynchronizationContext.Run();
-        } finally {
-            File.Delete(traceLogPath);
+        using var single = new SingleInstance("{B27A618B-BF63-4DE1-894A-D3A696402174}");
+        if (!single.IsTaken) {
+            return 0;
         }
+
+        using var mainWindow = new MainWindow();
+        return MessageLoopSynchronizationContext.Run();
     }
 }
