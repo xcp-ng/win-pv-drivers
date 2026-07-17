df0259edc091931f2817a839d305a8b6d640eaca
Tu Dinh <ngoc-tu.dinh@vates.tech>
2026-07-17 09:48:09 +0200

    update comment of XenIfaceHandle
    Signed-off-by: Tu Dinh <ngoc-tu.dinh@vates.tech>

--- xenplus/XenIface/XenIfaceHandle.cs
+++ xenplus/XenIface/XenIfaceHandle.cs
@@ -6,10 +6,11 @@ namespace XenPlus.XenIface;
 internal readonly ref struct XenIfaceHandle(XenIfaceSource parent) : IDisposable {
     /// <remarks>
     /// <para>
-    /// Both <c>class</c> and <c>ref struct</c> are inappropriate for <see cref="XenIfaceHandle"/> alone, the former
-    /// because the handle can escape its scope/thread (and therefore violate the thread requirements of
-    /// <see cref="Monitor"/>), and the latter because the handle can be copied. So we use a <c>class</c> wrapped by
-    /// a <c>ref struct</c> to provide both guarantees.
+    /// Both <see langword="class"/> and <see langword="ref"/> <see langword="struct"/> are inappropriate for
+    /// <see cref="XenIfaceHandle"/> alone, the former because the handle can escape its scope/thread (and therefore
+    /// violate the thread requirements of <see cref="Monitor"/>), and the latter because the handle can be copied. So
+    /// we use a <see langword="class"/> wrapped by a <see langword="ref"/> <see langword="struct"/> to provide both
+    /// guarantees.
     /// </para>
     /// <para>
     /// Note that the inner class doesn't make the handle uncopyable, but rather copy-safe.
