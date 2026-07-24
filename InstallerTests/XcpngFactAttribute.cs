3aad41b5c826e33316248a3eb16cda04fe7bfdd2
Tu Dinh <ngoc-tu.dinh@vates.tech>
2026-07-24 15:04:02 +0200

    fix and uniformize the InstallerTests project
    Signed-off-by: Tu Dinh <ngoc-tu.dinh@vates.tech>

--- InstallerTests/XcpngFactAttribute.cs
+++ InstallerTests/XcpngFactAttribute.cs
@@ -1,11 +1,20 @@
+using System.Runtime.CompilerServices;
 using Xunit;
 
 namespace InstallerTests {
     public sealed class XcpngFactAttribute : FactAttribute {
+        public XcpngFactAttribute(
+            [CallerFilePath] string? sourceFilePath = null,
+            [CallerLineNumber] int sourceLineNumber = -1)
+            : base(sourceFilePath, sourceLineNumber) {
+        }
+
         public XcpngFactAttribute() {
+#pragma warning disable CS0162 // Unreachable code detected
             if (XenDriverUtils.VersionInfo.VendorPrefix != "XN") {
                 Skip = "Not an XCP-ng build";
             }
+#pragma warning restore CS0162 // Unreachable code detected
         }
     }
 }
